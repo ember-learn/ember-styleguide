8722bfdb9d012709d7be056f028715d73d9dcd23
Melanie Sumner <melaniersumner@gmail.com>
2017-07-04 18:44:21 -0500

    dummy site work

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,6 +7,33 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('learn', function() {
+    this.route('quick-start');
+    this.route('tutorial');
+    this.route('deprecation');
+    this.route('showcase');
+  });
+  this.route('guides');
+  this.route('api', function() {
+    this.route('ember-data');
+  });
+  this.route('community', function() {
+    this.route('meetups');
+    this.route('mascots');
+    this.route('guidelines');
+    this.route('logos');
+  });
+  this.route('blog');
+  this.route('builds', function() {
+    this.route('canary');
+    this.route('beta');
+    this.route('release');
+  });
+  this.route('team');
+  this.route('sponsors');
+  this.route('security');
+  this.route('legal');
+  this.route('ember-users');
 });
 
 export default Router;
