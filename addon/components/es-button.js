36f10bb28210fff1b0a07723dedf08252ab4cbfc
Paul Bishop <pbishop16@gmail.com>
2017-12-29 14:39:19 -0600

    Base es-button

--- addon/components/es-button.js
+++ addon/components/es-button.js
@@ -1,21 +1,76 @@
 import Component from '@ember/component';
 import layout from '../templates/components/es-button';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  layout,
-  classNames: ['es-button'],
-  classNameBindings: ['isBlock:button-block'],
-  tagName: 'button',
+    layout,
+    tagName: 'button',
 
-  //accessibility support
-  ariaDescribedby: null,
-  ariaLabel: null,
-  ariaPressed: null,
-  title: null,
+    classNames: ['es-button'],
+    classNameBindings: [
+      'buttonIcon',
+      'buttonDark:button-dark',
+      'buttonLight:button-light',
+      'buttonBlock:button-block',
+      'buttonDense:button-dense',
+      'buttonTiny:button-tiny',
+      'buttonLink:button-link',
+      'icon:button-icon',
+    ],
+    attributeBindings: [
+      'ariaDisabled:aria-disabled',
+      'ariaDescribedby:aria-describedby',
+      'ariaLabel:aria-label',
+      'ariaPressed:aria-pressed',
+      'dataRole:data-role',
+      'disabled',
+      'ariaRole:role',
+      'title',
+      'type'
+    ],
 
-  //icon support
-  hasIcon: false,
-  iconUrl: null,
+    //attributes
+    dataRole: null,
+    label: null,
+    type: null,
 
-  isBlock: false
+    //sizes
+    buttonBlock: false,
+    buttonDense: false,
+    buttonLink: false,
+    buttonTiny: false,
+
+    //style
+    buttonDark: false,
+    buttonLight: false,
+    disabled: false,
+
+    //accessibility support
+    ariaDescribedby: null,
+    ariaDisabled: false,
+    ariaLabel: null,
+    ariaPressed: null,
+    ariaRole: 'button',
+    title: null,
+
+    //icon support
+    icon: null,
+
+    buttonIcon: computed('icon', function() {
+      const icon = this.get('icon');
+
+      if (icon) {
+        return `fa ${this.get('icon')}`;
+      }
+
+      return null;
+    }),
+
+    click() {
+      const onClicked = this.get('onClicked');
+
+      if (typeof(onClicked) === 'function') {
+        onClicked();
+      }
+    },
 });
--- tests/integration/components/es-button-test.js
+++ tests/integration/components/es-button-test.js
@@ -1,25 +1,218 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import { run } from '@ember/runloop';
 import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
 
 moduleForComponent('es-button', 'Integration | Component | es button', {
   integration: true
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{es-button}}`);
 
   assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
   this.render(hbs`
     {{#es-button}}
       template block text
     {{/es-button}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(
+    this.$().text().trim(),
+    'template block text'
+  );
+});
+
+test('has html button tag and base class', function(assert) {
+  this.render(hbs`{{es-button}}`);
+
+  assert.ok(
+    this.$('button'),
+    'has button tag'
+  );
+
+  assert.ok(
+    this.$('button').hasClass('es-button'),
+    'has base es-button class'
+  );
+});
+
+test('button supports font-awesome icon', function(assert) {
+  const icon = 'fa-exit';
+
+  this.setProperties({
+    icon,
+  });
+
+  this.render(hbs`
+    {{es-button
+      icon=icon
+    }}
+  `);
+
+  assert.ok(
+    this.$('button').hasClass(icon),
+    'has set icon class'
+  );
+
+  assert.ok(
+    this.$('button').hasClass('button-icon'),
+    'has class button-icon'
+  );
+});
+
+test('can set button-block class', function(assert) {
+  this.render(hbs`
+    {{es-button
+      buttonBlock=true
+    }}
+  `);
+
+  assert.ok(
+    this.$('button').hasClass('button-block'),
+    'has button-block class'
+  );
+});
+
+test('can display set label', function(assert) {
+  const label = 'Button Label';
+
+  this.setProperties({
+    label,
+  });
+
+  this.render(hbs`
+    {{es-button
+      label=label
+    }}
+  `);
+
+  assert.equal(
+    this.$('button').text().trim(),
+    label,
+    'displays button label'
+  );
+});
+
+test('calls closure function when clicked', function(assert) {
+  const onClicked = sinon.spy();
+
+  this.setProperties({
+    onClicked,
+  });
+
+  this.render(hbs`
+    {{es-button
+      onClicked=onClicked
+    }}
+  `);
+
+  run(() => {
+    this.$('button').trigger('click');
+  });
+
+  assert.ok(
+    onClicked.calledOnce,
+    'onClicked called'
+  );
+});
+
+test('can disable button', function(assert) {
+  const disabled = false;
+
+  this.setProperties({
+    disabled,
+  });
+
+  this.render(hbs`
+    {{es-button
+      disabled=disabled
+    }}
+  `);
+
+  assert.ok(
+    this.$('button').is(':enabled'),
+    'button is disabled'
+  );
+
+  run(() => {
+    this.set('disabled', true);
+  });
+
+  assert.ok(
+    this.$('button').is(':disabled'),
+    'button is disabled'
+  );
+});
+
+test('displays set data-role', function(assert) {
+  const dataRole = 'some-data-role';
+
+  this.setProperties({
+    dataRole,
+  });
+
+  this.render(hbs`{{es-button}}`);
+
+  assert.ok(
+    this.$(`[data-role=${dataRole}]`),
+    'set data-role is displayed'
+  );
+});
+
+test('can display default and set ariaRole', function(assert) {
+  const ariaRole = 'some-aria-role';
+
+  this.setProperties({
+    ariaRole,
+  });
+
+  this.render(hbs`
+    {{es-button
+      ariaRole=ariaRole
+    }}
+  `);
+
+  assert.ok(
+    this.$(`[role=${ariaRole}]`),
+    'set role is displayed'
+  );
+
+  run(() => {
+    this.set('ariaRole', null);
+  });
+
+  assert.ok(
+    this.$('[role=button]'),
+    'default role is displayed'
+  );
+});
+
+test('does not render aria-pressed unless set', function(assert) {
+  const ariaPressed = 'false';
+
+  this.setProperties({
+    ariaPressed,
+  });
+
+  this.render(hbs`
+    {{es-button
+      ariaPressed=ariaPressed
+    }}
+  `);
+
+  assert.ok(
+    this.$(`button`).attr('aria-pressed'),
+    'does have aria-pressed attribute'
+  );
+
+  run(() => {
+    this.set('ariaPressed', null);
+  });
+
+  assert.notOk(
+    this.$('button').attr('aria-pressed'),
+    'does not have aria-pressed attribute'
+  );
 });
