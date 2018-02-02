633c682a882a87b48c7ab33f5c568bf1e7c4f717
Paul Bishop <pbishop16@gmail.com>
2018-02-01 20:37:49 -0600

    Base es-button component (#11)

--- addon/components/es-button.js
+++ addon/components/es-button.js
@@ -1,21 +1,76 @@
 import Component from '@ember/component';
 import layout from '../templates/components/es-button';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  layout,
-  classNames: ['es-button'],
-  classNameBindings: ['isBlock:button-block'],
-  tagName: 'button',
+    layout,
+    tagName: 'button',
 
-  //accessibility support
-  ariaDescribedby: null,
-  ariaLabel: null,
-  ariaPressed: null,
-  title: null,
+    classNames: ['es-button'],
+    classNameBindings: [
+      'buttonIcon',
+      'isDark:button-dark',
+      'isLight:button-light',
+      'isBlock:button-block',
+      'isDense:button-dense',
+      'isTiny:button-tiny',
+      'isLink:button-link',
+      'icon:button-icon',
+    ],
+    attributeBindings: [
+      'ariaDisabled:aria-disabled',
+      'ariaDescribedby:aria-describedby',
+      'ariaLabel:aria-label',
+      'ariaPressed:aria-pressed',
+      'dataRole:data-role',
+      'disabled',
+      'ariaRole:role',
+      'title',
+      'type'
+    ],
 
-  //icon support
-  hasIcon: false,
-  iconUrl: null,
+    //attributes
+    dataRole: null,
+    label: null,
+    type: null,
 
-  isBlock: false
+    //sizes
+    isBlock: false,
+    isDense: false,
+    isLink: false,
+    isTiny: false,
+
+    //style
+    isDark: false,
+    isLight: false,
+    disabled: false,
+
+    //accessibility support
+    ariaDescribedby: null,
+    ariaDisabled: false,
+    ariaLabel: null,
+    ariaPressed: null,
+    ariaRole: 'button',
+    title: null,
+
+    //icon support
+    icon: null,
+
+    buttonIcon: computed('icon', function() {
+      const icon = this.get('icon');
+
+      if (icon) {
+        return `fa ${this.get('icon')}`;
+      }
+
+      return null;
+    }),
+
+    click() {
+      const onClicked = this.get('onClicked');
+
+      if (typeof(onClicked) === 'function') {
+        onClicked();
+      }
+    },
 });
