ba977263a75b2454e80a584fccfdfcb70f7bcc7a
simonihmig <ihmig@kaliber5.de>
2018-02-11 01:15:24 +0100

    Import Bootstrap Sass

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 'use strict';
+const mergeTrees = require('broccoli-merge-trees');
+const Funnel = require('broccoli-funnel');
+const path = require('path');
 
 module.exports = {
   name: 'ember-styleguide',
 
   included() {
     this._super.included.apply(this, arguments);
+  },
+
+  treeForAddonStyles(tree) {
+    let bootstrapTree = new Funnel(this.getBootstrapStylesPath(), {
+      destDir: 'ember-bootstrap'
+    });
+    return mergeTrees([bootstrapTree, tree]);
+  },
+
+  getBootstrapStylesPath() {
+    let pkgPath = path.dirname(require.resolve(`bootstrap/package.json`));
+    return path.join(pkgPath, 'scss');
   }
 };
