0543e349f088262ba9cd8fc60de7ac5132da07c2
Melanie Sumner <melaniersumner@gmail.com>
2018-02-19 18:02:44 -0600

    moved the location of the data folder to the dummy app

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import links from 'ember-styleguide/data/navbar-links';
+import links from '../data/navbar-links';
 
 export default Route.extend({
   model() {
