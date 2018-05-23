aac203116e35b0d2b0597e9eb7bcdcd5d6f07100
Daniel Steshenko <dan.steshenko@gmail.com>
2018-05-23 23:42:50 +0200

    Bugfix/delete original bootstrap things (#52)
    * Delete bootstrap related things #51
    
    * cleanup ember-cli-build #51
    
    * move bootstrap imports to main scss file #51
    
    * Move back brocolli tree hacks #51

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,14 +5,12 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     'ember-bootstrap': {
-      'bootstrapVersion': 4,
-      'importBootstrapFont': false,
-      'importBootstrapCSS': false
+      bootstrapVersion: 4,
+      importBootstrapFont: false,
+      importBootstrapCSS: false
     },
     svgJar: {
-      sourceDirs: [
-        'tests/dummy/public/assets/images/icons'
-      ]
+      sourceDirs: ['tests/dummy/public/assets/images/icons']
     }
   });
 
