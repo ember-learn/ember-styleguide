ca81e11e2555bf978c9ec50e7cabb7d4691e8f58
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-05-27 22:21:35 +0530

    Use yarn & target recent ember versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,18 +1,11 @@
 module.exports = {
+  useYarn: true,
   scenarios: [
     {
-      name: 'ember-lts-2.12',
+      name: 'ember-lts-2.18',
       npm: {
         devDependencies: {
-          'ember-source': '~2.12.0'
-        }
-      }
-    },
-    {
-      name: 'ember-lts-2.16',
-      npm: {
-        devDependencies: {
-          'ember-source': '~2.16.0'
+          'ember-source': '~2.18.0'
         }
       }
     },
