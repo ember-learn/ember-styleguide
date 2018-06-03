39553c5d485b934e88f34ad44f7e8c852efad4c0
Daniel Steshenko <dan.steshenko@gmail.com>
2018-06-03 22:27:54 +0200

    Rewrite es-card

--- addon/components/es-card.js
+++ addon/components/es-card.js
@@ -1,16 +1,36 @@
 import Component from '@ember/component';
 import layout from '../templates/components/es-card';
+import { computed, get } from '@ember/object';
 
 export default Component.extend({
   layout,
-  classNames: ['es-card'],
-  classNameBindings: ['hasBorder:border'],
 
-  //accessibility support
+  classNames: ['es-card'],
+  classNameBindings: [
+    'isImageLeft:es-card--image-left',
+    'isImageRight:es-card--image-right'
+  ],
+  attributeBindings: ['borderColorCSS:style'],
+
+  // accessibility support
   ariaDescribedby: null,
   ariaLabel: null,
   ariaRole: null,
   title: null,
 
-  hasBorder: false
+  // passed in attrs
+  heading: null,
+  text: null,
+  imageSrc: null,
+  buttonLabel: null,
+  borderColor: null,
+  isImageLeft: false,
+  isImageRight: false,
+  isHeadingAbove: false,
+
+  borderColorCSS: computed({
+    get() {
+      return `border-color: ${get(this, 'borderColor')}`;
+    }
+  })
 });
