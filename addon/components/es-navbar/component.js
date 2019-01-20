e160f14384b21f1edf971e06b49c5640423f2490
Chris Manson <chris@manson.ie>
2019-01-20 12:17:42 +0000

    allowing the ability to override logo link

--- addon/components/es-navbar/component.js
+++ addon/components/es-navbar/component.js
@@ -6,6 +6,13 @@ import defaultLinks from '../../constants/links';
 
 export default Component.extend({
   layout,
+  navHome: computed('home', function() {
+    if (this.home) {
+      return this.home;
+    }
+
+    return 'https://www.emberjs.com';
+  }),
   navLinks: computed('links.[]', function() {
     if(this.links) {
       return this.links;
