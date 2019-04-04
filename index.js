8f26c8020a7fd7a62bf0c7a91d779ad9442adadd
Chris Manson <chris@manson.ie>
2019-04-04 21:37:37 +0100

    moving over to postcss

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 const Funnel = require('broccoli-funnel');
 const path = require('path');
-const nodeSass = require('node-sass');
+
+const CssImport = require('postcss-import')
+const PresetEnv = require('postcss-preset-env');
 
 module.exports = {
   name: require('./package').name,
@@ -21,37 +23,18 @@ module.exports = {
     googleFonts: [
       'Roboto:400,700'
     ],
-  },
-
-  included(app, parentAddon) {
-    let target = (app || parentAddon);
-    target.options = target.options || {};
-
-    target.options.sassOptions = target.options.sassOptions || { implementation: nodeSass };
-
-    this.checkPreprocessor();
-
-    this._super.included.apply(this, arguments);
-  },
-
-  treeForStyles() {
-    return new Funnel(this.getEmberStyleguideStylesPath(), {
-      destDir: 'ember-styleguide'
-    });
+    postcssOptions: {
+      compile: {
+        enabled: true,
+        plugins: [
+          { module: CssImport },
+          { module: PresetEnv({ stage: 3 }) }
+        ]
+      }
+    }
   },
 
   treeForPublic: function() {
     return new Funnel(path.join(this.root, 'public'));
   },
-
-  getEmberStyleguideStylesPath() {
-    let pkgPath = path.dirname(__filename);
-    return path.join(pkgPath, 'addon', 'styles');
-  },
-
-  checkPreprocessor() {
-    if (this.app && !this.app.project.findAddonByName('ember-cli-sass')) {
-      this.ui.writeLine('ember-styleguide: npm package "ember-cli-sass" is missing. Consider using it to use `@import \'ember-styleguide/globals/variables\'` in your styles.');
-    }
-  }
 };
