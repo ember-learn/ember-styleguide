ede62f0e1b35e9d93c766271a9eb36a454f1dda8
Chris Manson <chris@manson.ie>
2019-04-06 15:28:13 +0100

    converting some of the components to use sparkles

--- addon/components/es-header.js
+++ addon/components/es-header.js
@@ -1,15 +1,5 @@
-import Component from '@ember/component';
-import layout from '../templates/components/es-header';
+import Component from 'sparkles-component';
 
-export default Component.extend({
-  layout,
-  attributeBindings: ['ariaLabel:aria-label'],
-  classNames: ['es-header'],
-  tagName: 'header',
+export default class EsHeader extends Component {
 
-  //accessibility support
-  ariaDescribedby: null,
-  ariaLabel: null,
-  ariaRole: 'banner',
-  title: null
-});
+}
---
+++ addon/components/es-page-header.js
@@ -0,0 +1,5 @@
+import Component from 'sparkles-component';
+
+export default class EsPageHeader extends Component {
+
+}
