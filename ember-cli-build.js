8d678e55155ec18b4014a83c879b1170739433f2
Chris Manson <chris@manson.ie>
2019-04-22 15:28:41 +0100

    implmenting markdown-templates for documentation

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -30,6 +30,9 @@ module.exports = function(defaults) {
         ]
       }
     },
+    'ember-cli-markdown-templates': {
+      syntaxHighlight: true,
+    }
   });
 
   /*
@@ -38,6 +41,8 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+
+  app.import('node_modules/highlightjs/styles/default.css');
 
   return app.toTree();
 };
---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,26 @@
+import Controller from '@ember/controller';
+
+const components = [
+  'accordion',
+  'aside',
+  'button',
+  'buttonbar',
+  'card',
+  'codesample',
+  'datatable',
+  'footer',
+  'form',
+  'header',
+  'heading',
+  'main',
+  'nav',
+  'navbar',
+  'note',
+  'pagewrapper',
+  'search',
+  'ulist',
+]
+
+export default Controller.extend({
+  components,
+})
