38c9cd4de3e7286fd289205dd76b8d669cbedee8
Chris Manson <chris@manson.ie>
2019-07-05 16:26:50 +0100

    fixing tests

--- addon/components/es-button.js
+++ addon/components/es-button.js
@@ -2,4 +2,19 @@ import Component from 'sparkles-component';
 
 export default class EsButton extends Component {
 
+  constructor({ onClicked }) {
+    super(...arguments);
+
+    if(!onClicked) {
+      // eslint-disable-next-line no-console
+      console.warn(new Error('Button created with no onClicked'));
+      this.onClicked = function() {};
+    } else {
+      this.onClicked = onClicked;
+    }
+  }
+
+  buttonClicked() {
+    this.onClicked();
+  }
 }
