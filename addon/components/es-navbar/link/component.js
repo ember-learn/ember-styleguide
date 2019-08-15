8c90e181ee90904738efba37e5a20d18c60cfc76
Chris Manson <chris@manson.ie>
2019-08-15 12:14:00 +0100

    add functionality to navbar

--- addon/components/es-navbar/link/component.js
+++ addon/components/es-navbar/link/component.js
@@ -3,7 +3,7 @@ import layout from './template';
 import { computed } from '@ember/object';
 import { equal } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
-import { next } from '@ember/runloop';
+import { schedule, next } from '@ember/runloop';
 
 export default Component.extend({
   layout,
@@ -12,229 +12,66 @@ export default Component.extend({
   classNameBindings: ['isDropdown:dropdown'],
   isDropdown: equal('link.type', 'dropdown'),
 
-  keyCode: Object.freeze({
-    'TAB': 9,
-    'RETURN': 13,
-    'ESC': 27,
-    'SPACE': 32,
-    'PAGEUP': 33,
-    'PAGEDOWN': 34,
-    'END': 35,
-    'HOME': 36,
-    'LEFT': 37,
-    'UP': 38,
-    'RIGHT': 39,
-    'DOWN': 40
+  isDropdownOpen: false,
+
+  // because aria-expanded requires a string value instead of a boolean
+  isExpanded: computed('isDropdownOpen', function() {
+    return this.isDropdownOpen ? 'true' : 'false';
   }),
 
-  navbar: service(),
+  navbar: service(), //TODO  also do we need this too?
 
-  didInsertElement() {
-    // this.element.tabIndex = -1;
+  actions: {
+    triggerDropdown() {
+      this.toggleProperty('isDropdownOpen');
 
-    this.get('navbar').register(this);
-    this.domNode = this.element.querySelector('ul.navbar-dropdown-list');
+      if (this.isDropdownOpen) {
+        // once it's open, let's make sure it can do some things
+        schedule('afterRender', this, function() {
+          // Define startup states
+          // Dropdown list
+          let dropdownList = this.element.querySelector('.navbar-dropdown-list');
 
-    if(this.domNode) {
-      this.element.querySelector('button').onmousedown = () => this.expand();
-      let links = Array.from(this.domNode.querySelectorAll('a'))
+          // Identify / set focus on the first item in the dropdown list automatically
+          let firstFocusable = this.element.querySelector('.navbar-dropdown-list li:first-of-type a');
+          firstFocusable.focus();
 
-      links.forEach((anchor) => {
-        anchor.addEventListener('blur', () => this.handleBlur());
-      });
+          // Need some event listeners for keyboard events
+          dropdownList.addEventListener('keydown', event => {
+
+            // Listen for ESC key to exit
+            if (event.keyCode === 27 && this.isDropdownOpen) {
+              this.closeDropdown();
+            } else if (this.isDropdownOpen) {
+              // if focus leaves the open dropdown, close it (without trying to otherwise control focus)
+              this.handleBlur();
+            } else {
+              return;
+            }
+          });
+        });
+      }
     }
+  },
+  closeDropdown() {
+    // set the isDropdownOpen to false, which will make the dropdown go away
+    this.set('isDropdownOpen', false);
+    // after that rendering bit happens, we need to return the focus to the trigger
+    schedule('afterRender', this, function() {
+      let dropdownTrigger = this.element.querySelector('.navbar-list-item-dropdown-toggle');
+      dropdownTrigger.focus();
+    });
   },
 
   handleBlur() {
     next(this, function() {
-      let subItems = Array.from(this.element.querySelectorAll('.navbar-dropdown-list-item'));
-      let focused = subItems.find(item => document.activeElement === item.querySelector('button'));
+      let subItems = Array.from(this.element.querySelectorAll('.navbar-dropdown-list li'));
+      let focused = subItems.find(item => document.activeElement === item.querySelector('a'));
 
-      // debugger
-      if(!focused) {
-        this.closePopupMenu();
+      if (!focused) {
+        this.set('isDropdownOpen', false);
       }
-    })
-  },
-
-  openPopupMenu() {
-    // Get position and bounding rectangle of controller object's DOM node
-    var rect = this.element.getBoundingClientRect();
-
-    // Set CSS properties
-    if(this.domNode) {
-      this.domNode.style.display = 'block';
-      this.domNode.style.top = rect.height + 'px';
-      this.domNode.style.zIndex = 10;
-    }
-
-    this.set('expanded', true);
-  },
-
-  closePopupMenu(force) {
-    var controllerHasHover = this.hasHover;
-
-    var hasFocus = this.hasFocus;
-
-    if (!this.isMenubarItem) {
-      controllerHasHover = false;
-    }
-
-    if (force || (!hasFocus && !this.hasHover && !controllerHasHover)) {
-      if(this.domNode) {
-        this.domNode.style.display = 'none';
-        this.domNode.style.zIndex = 0;
-      }
-      this.set('expanded', false);
-    }
-  },
-
-  expanded: computed({
-    get() {
-      return this.element.getAttribute('aria-expanded') === 'true';
-    },
-    set(key, value) {
-      this.element.setAttribute('aria-expanded', value);
-    }
-  }).volatile(),
-
-  setFocusToFirstItem() {
-    let element = this.element.querySelector('.navbar-dropdown-list-item-link')
-    if (element) {
-      element.focus();
-    }
-  },
-
-  setFocusToLastItem() {
-    this.element.querySelector('ul[role="menu"] li a:last-of-type').focus();
-  },
-
-  setFocusToNextItem() {
-    let subItems = Array.from(this.element.querySelectorAll('ul[role="menu"] li'));
-
-    let focused = subItems.find(item => document.activeElement === item.querySelector('a'));
-    let focusedIndex = subItems.indexOf(focused);
-
-    let nextItem = subItems[(focusedIndex + 1) % subItems.length];
-
-    if (!nextItem) {
-      return;
-    }
-
-    nextItem.querySelector('a').focus();
-  },
-
-  setFocusToPreviousItem() {
-    let subItems = Array.from(this.element.querySelectorAll('ul[role="menu"] li'));
-
-    let focused = subItems.find(item => document.activeElement === item.querySelector('a'));
-    let focusedIndex = subItems.indexOf(focused);
-
-    let nextIndex = focusedIndex - 1;
-
-    if (nextIndex < 0) {
-      nextIndex = subItems.length - 1;
-    }
-
-    let nextItem = subItems[nextIndex];
-
-    if (!nextItem) {
-      return;
-    }
-
-    nextItem.querySelector('a').focus();
-  },
-
-  keyDown(event) {
-    let flag = false;
-    let clickEvent;
-    let mousedownEvent;
-
-    switch (event.keyCode) {
-      case this.keyCode.RETURN:
-      case this.keyCode.SPACE:
-        // Create simulated mouse event to mimic the behavior of ATs
-        // and let the event handler handleClick do the housekeeping.
-        mousedownEvent = new MouseEvent('mousedown', {
-          'view': window,
-          'bubbles': true,
-          'cancelable': true
-        });
-        clickEvent = new MouseEvent('click', {
-          'view': window,
-          'bubbles': true,
-          'cancelable': true
-        });
-
-        document.activeElement.dispatchEvent(mousedownEvent);
-        document.activeElement.dispatchEvent(clickEvent);
-
-        flag = true;
-        break;
-      case this.keyCode.DOWN:
-        if(this.get('expanded')) {
-          this.setFocusToNextItem();
-        } else {
-          this.openPopupMenu();
-          this.setFocusToFirstItem();
-        }
-        flag = true;
-        break;
-
-      case this.keyCode.LEFT:
-        this.get('navbar').setFocusToPreviousItem(this);
-        flag = true;
-        break;
-
-      case this.keyCode.RIGHT:
-        this.get('navbar').setFocusToNextItem(this);
-        flag = true;
-        break;
-
-      case this.keyCode.UP:
-        if(this.get('expanded')) {
-          this.setFocusToPreviousItem();
-        } else {
-          this.openPopupMenu();
-          this.setFocusToLastItem();
-        }
-        break;
-
-      case this.keyCode.HOME:
-      case this.keyCode.PAGEUP:
-        this.setFocusToFirstItem();
-        flag = true;
-        break;
-
-      case this.keyCode.END:
-      case this.keyCode.PAGEDOWN:
-        this.setFocusToLastItem();
-        flag = true;
-        break;
-
-      case this.keyCode.TAB:
-        this.closePopupMenu(true);
-        break;
-
-      case this.keyCode.ESC:
-        this.closePopupMenu(true);
-        break;
-    }
-
-    if (flag) {
-      event.stopPropagation();
-      event.preventDefault();
-    }
-  },
-
-  expand() {
-    next(this, () => {
-      if(this.get('expanded')) {
-        this.closePopupMenu();
-      } else {
-        this.openPopupMenu();
-        this.setFocusToFirstItem();
-      }
-    })
+    });
   }
+
 });
