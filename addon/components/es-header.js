bc83fd518e2693fdd6449bf15dc7c00268f3270a
Chris Manson <chris@manson.ie>
2020-03-31 10:41:02 +0100

    convert es-header to a glimmer component

--- addon/components/es-header.js
+++ addon/components/es-header.js
@@ -1,9 +1,9 @@
-import Component from 'sparkles-component';
 import { set } from '@ember/object';
+import Component from '@glimmer/component';
 
 import defaultLinks from '../constants/links';
 
-export default class EsHeader extends Component {
+export default class EsHeaderComponent extends Component {
   expanded = false;
 
   get navHome() {
@@ -26,7 +26,7 @@ export default class EsHeader extends Component {
     set(this, 'expanded', !this.expanded);
   }
 
-  didInsertElement() {
+  trackExpanded() {
     // Ensure menu is marked as expanded if there is enough screen estate
     // TODO: Dynamically calculate necessary horizontal space and collapse based on that
     if (typeof FastBoot === 'undefined') {
