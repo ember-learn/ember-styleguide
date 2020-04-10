658c6d21635a320d9657b58ca501fe811748e584
Alexey Kostevich <alexey.kostevich@outlook.com>
2020-04-10 22:48:38 +1000

    Simplify `EsHeaderComponent` implementation.
    - Update the component to use Ember Octane API.
    - Remove the code for reseting `expanded` on CSS breakpoints for
      a more predictable behaviour and prevention of memory leaks.
    - Add `navbar-expanded` class to simplify styles and reduce coupling
      with HTML structure.
    - Fix accessibility warnings.
    - Add additional tests.

--- tests/integration/components/es-header-test.js
+++ tests/integration/components/es-header-test.js
@@ -1,43 +1,69 @@
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
+import { setProperties } from '@ember/object';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
+const customHomeUrl = 'https://github.com/emberjs';
+
 module('Integration | Component | es header', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
+  test('it uses the header HTML element with correct attributes', async function(assert) {
+    await render(hbs`<EsHeader/>`);
 
-    await render(hbs`{{es-header}}`);
+    assert.dom('header').exists({ count: 1 });
+    assert.dom('header').hasClass('es-header');
+    assert.dom('header').hasAttribute('role', 'banner');
+  });
 
-    assert.dom(this.element).containsText('Show Site Navigation');
+  test('it renders the logo', async function(assert) {
+    await render(hbs`<EsHeader/>`);
 
-    // Template block usage:
+    assert.dom('.navbar-brand').hasAttribute('src', '/images/ember-logo.svg');
+  });
+
+  test('it renders the link to the Ember homepage by default', async function(assert) {
+    await render(hbs`<EsHeader/>`);
+
+    assert.dom('.navbar-brand-wrapper').hasAttribute('href', 'https://www.emberjs.com');
+  });
+
+  test('it renders a link to a custom homepage', async function(assert) {
+    setProperties(this, { customHomeUrl });
+    await render(hbs`<EsHeader @home={{customHomeUrl}} />`);
+
+    assert.dom('.navbar-brand-wrapper').hasAttribute('href', customHomeUrl);
+  });
+
+  test('it renders custom content at the end', async function(assert) {
     await render(hbs`
-      {{#es-header}}
-        template block text
-      {{/es-header}}
+      <EsHeader>
+        Custom content
+      </EsHeader>
     `);
 
-    assert.dom(this.element).containsText('template block text');
+    assert.dom('.navbar-end').containsText('Custom content');
   });
 
-  test('it uses the header html element', async function(assert) {
-    await render(hbs`{{es-header}}`);
-    assert.dom('header').exists({ count: 1 }, 'the header uses the header html element!');
+  test('it renders the navbar collapsed by default', async function(assert) {
+    await render(hbs`<EsHeader/>`);
+
+    assert.dom('.es-navbar').doesNotHaveClass('navbar-expanded');
+    assert.dom('.navbar-toggler').containsText('Show Site Navigation');
   });
 
-  test('it has the role of banner', async function(assert) {
-    await render(hbs`{{es-header}}`);
-    assert.dom('header').hasAttribute('role', 'banner', 'header has the role of banner');
-  });
+  test('it toggles the navbar when click the toggler', async function(assert) {
+    await render(hbs`<EsHeader/>`);
 
-  //the class matches the component name
-  //but how do I make it so it just checks for the one of them?
-  test('it has the className es-header', async function(assert) {
-    await render(hbs`{{es-header}}`);
-    assert.dom('header').hasClass('es-header');
+    await click('.navbar-toggler');
+
+    assert.dom('.es-navbar').hasClass('navbar-expanded');
+    assert.dom('.navbar-toggler').containsText('Hide Site Navigation');
+
+    await click('.navbar-toggler');
+
+    assert.dom('.es-navbar').doesNotHaveClass('navbar-expanded');
+    assert.dom('.navbar-toggler').containsText('Show Site Navigation');
   });
 });
