0eb219ff4c82b3629f73d30dd710cb33d33935e6
Chris Manson <chris@manson.ie>
2020-04-19 11:13:06 +0100

    fix lint on es-button

--- addon/components/es-button.js
+++ addon/components/es-button.js
@@ -1,8 +1,10 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class EsButtonComponent extends Component {
   // default value
   _onClicked = () => {};
+  _type = "button";
 
   constructor() {
     super(...arguments);
@@ -14,8 +16,16 @@ export default class EsButtonComponent extends Component {
     } else {
       this._onClicked = this.args.onClicked;
     }
+
+    if(!this.args.type) {
+      // eslint-disable-next-line no-console
+      console.warn(new Error('Button created with no @type defined - defaulting to `type="button"`'));
+    } else {
+      this._type = this.args.type;
+    }
   }
 
+  @action
   buttonClicked() {
     this._onClicked();
   }
