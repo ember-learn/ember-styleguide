1725412452cb6840c30135de9453547bce7ca10c
Isaac Lee <ijlee2@users.noreply.github.com>
2021-03-18 09:12:58 +0100

    Enabled Prettier

--- addon/constants/links.js
+++ addon/constants/links.js
@@ -1,128 +1,167 @@
-/* eslint-disable prettier/prettier */
-export default [{
-  name: 'Docs',
-  type: 'dropdown',
-  items: [{
-    href: 'https://guides.emberjs.com/release/',
-    name: 'Ember.js Guides',
-    type: 'link'
-  }, {
-    href: 'https://api.emberjs.com',
-    name: 'API Reference',
-    type: 'link'
-  }, {
-    href: 'https://cli.emberjs.com',
-    name: 'CLI Guides',
-    type: 'link'
-  }, {
-    type: 'divider'
-  }, {
-    href: 'https://emberjs.com/learn',
-    name: 'Learn Ember',
-    type: 'link'
-  }]
-}, {
-  name: 'Releases',
-  type: 'dropdown',
-  items: [{
-    href: 'https://emberjs.com/releases',
-    name: 'Overview',
-    type: 'link'
-  }, {
-    href: 'https://emberjs.com/releases/lts',
-    name: '→ LTS',
-    type: 'link'
-  }, {
-    href: 'https://emberjs.com/releases/release',
-    name: '→ Stable',
-    type: 'link'
-  }, {
-    href: 'https://emberjs.com/releases/beta',
-    name: '→ Beta',
-    type: 'link'
-  }, {
-    href: 'https://emberjs.com/releases/canary',
-    name: '→ Canary',
-    type: 'link'
-  }, {
-    type: 'divider'
-  }, {
-    href: 'https://emberjs.com/editions',
-    name: 'Editions',
-    type: 'link'
-  }, {
-    href: 'https://deprecations.emberjs.com',
-    name: 'Deprecations',
-    type: 'link'
-  }]
-}, {
-  href: 'https://blog.emberjs.com',
-  name: 'Blog',
-  type: 'link'
-}, {
-  name: 'Community',
-  type: 'dropdown',
-  items: [{
-    href: 'https://emberjs.com/community',
-    name: 'The Ember Community',
-    type: 'link'
-  }, {
-    href: 'https://emberjs.com/guidelines',
-    name: 'Guidelines',
-    type: 'link'
-  }, {
-    href: 'https://help-wanted.emberjs.com/',
-    name: 'Help Wanted',
-    type: 'link'
-  }, {
-    type: 'divider'
-  }, {
-    href: 'https://emberjs.com/community/meetups',
-    name: 'Meetups',
-    type: 'link'
-  }, {
-    type: 'divider'
-  }, {
-    href: 'http://emberconf.com/',
-    name: 'Ember Conf',
-    type: 'link'
-  }]
-}, {
-  name: 'About',
-  type: 'dropdown',
-  items: [{
-    href: 'https://emberjs.com/team',
-    name: 'The Team',
-    type: 'link'
-  }, {
-    type: 'divider'
-  }, {
-    href: 'https://emberjs.com/logos',
-    name: 'Branding',
-    type: 'link'
-  }, {
-    href: 'https://emberjs.com/mascots',
-    name: 'Mascots',
-    type: 'link'
-  }, {
-    type: 'divider'
-  }, {
-    href: 'https://emberjs.com/ember-users',
-    name: 'Who Uses Ember',
-    type: 'link'
-  }, {
-    href: 'https://emberjs.com/sponsors',
-    name: 'Sponsors',
-    type: 'link'
-  }, {
-    type: 'divider'
-  }, {
-    href: 'https://emberjs.com/about/legal',
-    name: 'Legal',
-    type: 'link'
-  }, {
-    href: 'https://emberjs.com/security',
-    name: 'Security',
-    type: 'link'
-  }]
-}];
+export default [
+  {
+    name: 'Docs',
+    type: 'dropdown',
+    items: [
+      {
+        href: 'https://guides.emberjs.com/release/',
+        name: 'Ember.js Guides',
+        type: 'link',
+      },
+      {
+        href: 'https://api.emberjs.com',
+        name: 'API Reference',
+        type: 'link',
+      },
+      {
+        href: 'https://cli.emberjs.com',
+        name: 'CLI Guides',
+        type: 'link',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        href: 'https://emberjs.com/learn',
+        name: 'Learn Ember',
+        type: 'link',
+      },
+    ],
+  },
+  {
+    name: 'Releases',
+    type: 'dropdown',
+    items: [
+      {
+        href: 'https://emberjs.com/releases',
+        name: 'Overview',
+        type: 'link',
+      },
+      {
+        href: 'https://emberjs.com/releases/lts',
+        name: '→ LTS',
+        type: 'link',
+      },
+      {
+        href: 'https://emberjs.com/releases/release',
+        name: '→ Stable',
+        type: 'link',
+      },
+      {
+        href: 'https://emberjs.com/releases/beta',
+        name: '→ Beta',
+        type: 'link',
+      },
+      {
+        href: 'https://emberjs.com/releases/canary',
+        name: '→ Canary',
+        type: 'link',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        href: 'https://emberjs.com/editions',
+        name: 'Editions',
+        type: 'link',
+      },
+      {
+        href: 'https://deprecations.emberjs.com',
+        name: 'Deprecations',
+        type: 'link',
+      },
+    ],
+  },
+  {
+    href: 'https://blog.emberjs.com',
+    name: 'Blog',
+    type: 'link',
+  },
+  {
+    name: 'Community',
+    type: 'dropdown',
+    items: [
+      {
+        href: 'https://emberjs.com/community',
+        name: 'The Ember Community',
+        type: 'link',
+      },
+      {
+        href: 'https://emberjs.com/guidelines',
+        name: 'Guidelines',
+        type: 'link',
+      },
+      {
+        href: 'https://help-wanted.emberjs.com/',
+        name: 'Help Wanted',
+        type: 'link',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        href: 'https://emberjs.com/community/meetups',
+        name: 'Meetups',
+        type: 'link',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        href: 'http://emberconf.com/',
+        name: 'Ember Conf',
+        type: 'link',
+      },
+    ],
+  },
+  {
+    name: 'About',
+    type: 'dropdown',
+    items: [
+      {
+        href: 'https://emberjs.com/team',
+        name: 'The Team',
+        type: 'link',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        href: 'https://emberjs.com/logos',
+        name: 'Branding',
+        type: 'link',
+      },
+      {
+        href: 'https://emberjs.com/mascots',
+        name: 'Mascots',
+        type: 'link',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        href: 'https://emberjs.com/ember-users',
+        name: 'Who Uses Ember',
+        type: 'link',
+      },
+      {
+        href: 'https://emberjs.com/sponsors',
+        name: 'Sponsors',
+        type: 'link',
+      },
+      {
+        type: 'divider',
+      },
+      {
+        href: 'https://emberjs.com/about/legal',
+        name: 'Legal',
+        type: 'link',
+      },
+      {
+        href: 'https://emberjs.com/security',
+        name: 'Security',
+        type: 'link',
+      },
+    ],
+  },
+];
