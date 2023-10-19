76e7062654c227510a184508867b5da76fc67c96
Chris Manson <chris@manson.ie>
2023-10-19 17:09:58 +0100

    update to v4.4 with ember-cli-update

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -7,7 +7,6 @@ module.exports = function (environment) {
     environment,
     rootURL: '/',
     locationType: 'history',
-
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
