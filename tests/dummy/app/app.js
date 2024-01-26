20ca9f262f4d4a2d9b4df8a010a4f0f2626b42e2
Chris Manson <chris@manson.ie>
2024-01-26 14:27:57 +0000

    fix ember-keyboard issue in docs

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,6 +3,8 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
 
+import "ember-keyboard/helpers/on-key.js"
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
