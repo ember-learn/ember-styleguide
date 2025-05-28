87d2aa8d478acebe82fb7542be68a7055f0fe053
Chris Manson <chris@manson.ie>
2025-05-28 13:07:47 +0100

    run lint-to-the-future ignore

--- tests/integration/components/es-footer-test.js
+++ tests/integration/components/es-footer-test.js
@@ -1,4 +1,4 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable prettier/prettier, qunit/no-assert-equal */
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
