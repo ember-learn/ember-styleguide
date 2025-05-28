c1c53380ffc4ea5b1cf749046a71fdd26baef2cf
Chris Manson <chris@manson.ie>
2025-05-28 13:32:03 +0100

    fix ember-try

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -12,6 +12,8 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+            'ember-cli': '~4.12.0',
+            'ember-resolver': '^11.0.0',
           },
         },
       },
@@ -20,6 +22,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.4.0',
+            'ember-resolver': '^11.0.0',
           },
         },
       },
@@ -28,6 +31,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.8.0',
+            'ember-resolver': '^11.0.0',
           },
         },
       },
